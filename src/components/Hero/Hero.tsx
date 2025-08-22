8b3edd55f3d54103a901500446d1cf3bdd6ef3a1
Andressa Duarte <andressaxduarte@gmail.com>
2025-08-22 11:34:28 -0300

    added local fonts

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import styles from "./Hero.module.scss";
 
@@ -17,24 +16,8 @@ export default function Hero() {
   return (
     <section className={styles.hero}>
       <div className={styles.content}>
-        <motion.h1
-          className={styles.title}
-          initial={{ opacity: 0, y: -10 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.05 }}
-        >
-          A Melhor Faculdade
-        </motion.h1>
-
-        <motion.h1
-          className={styles.title}
-          initial={{ opacity: 0, y: -10 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.2, delay: 0.05 }}
-        >
-          de Tecnologia
-        </motion.h1>
-
+        <h1 className={styles.title}>A Melhor Faculdade</h1>
+        <h1 className={styles.title}>de Tecnologia</h1>
         <p className={styles.subtitle}>
           {isMobile ? (
             <>
